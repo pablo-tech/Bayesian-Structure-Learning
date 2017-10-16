00ce1e6eeb2ec6e3d1382bc874d3b065b93710dd
pablo <pablo.personal@icloud.com>
2017-10-16 15:58:58 -0700

    added file reader and writer

--- grapho.py
+++ grapho.py
@@ -18,11 +18,38 @@ except RuntimeError:
     print("Matplotlib unable to open")
     raise
 
+# CSV
+try:
+    import csv
+except ImportError:
+    raise ImportError("CSV required for read()")
+except RuntimeError:
+    print("CSV unable to open")
+    raise
+
+
 # Directed graphs, that is, graphs with directed edges
 G=nx.Graph()
-
-G2=nx.DiGraph()
+G=nx.DiGraph()
 
 G.add_edge(2,3,weight=0.9)
+G.add_nodes_from([2,3])
+
+print(G.adj)
+
+# READER
+with open('small.csv', 'rb') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csv_reader:
+        print ', '.join(row)
+
+
+# WRITER
+with open('eggs.csv', 'wb') as csvfile:
+    gph_writer = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    gph_writer.writerow(['Spam'] * 5 + ['Baked Beans'])
+    gph_writer.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
 
 
