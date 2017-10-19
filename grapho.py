e5d7d6f764be710e0c87cf9e090a7ded8213a2c2
pablo <pablo.bertorello@gmail.com>
2017-10-18 17:28:48 -0700

    per K2 algo now looking for parent for the current node

--- grapho.py
+++ grapho.py
@@ -69,7 +69,7 @@ def optimizeGraph(graph, dataframe):
 # greedy iteration over the graph looking for a better shape than current
 # attempt the modifiaction at the provided node
 # try to connect the provided node to a random other node
-def getChangedGraph(graph, fromNode, dataframe):
+def getChangedGraph(graph, toNode, dataframe):
     maxTries = 100
     attempt = 0
     minScoreGain = 10
@@ -78,25 +78,25 @@ def getChangedGraph(graph, fromNode, dataframe):
     while True:
         randomNode = choice(graph.nodes())
         attempt = attempt + 1
-        print "Attempt=" + str(attempt) + " at optimizing from node=" + str(fromNode)
+        print "Attempt=" + str(attempt) + " at optimizing to node=" + str(toNode)
         if attempt>maxTries:
             if int(oscore.getScore(bestMoveGaph, dataframe, attempt)) > int(oscore.getScore(initialGraph, dataframe, attempt)):
-                print "FOUND a graph optimization from node=" + str(fromNode) + "... " + str(oshow.toEdgesString(bestMoveGaph))
+                print "FOUND a graph optimization to node=" + str(toNode) + "... " + str(oshow.toEdgesString(bestMoveGaph))
                 return bestMoveGaph # after many attempts, return the bestMoveGaph
             else:
-                print "NO better graph found from node=" + str(fromNode) + "... " + str(oshow.toEdgesString(initialGraph))
+                print "NO better graph found to node=" + str(toNode) + "... " + str(oshow.toEdgesString(initialGraph))
                 return initialGraph
         # if randomNode!=fromNode:   # connect only to a different node
-        if not graph.has_edge(fromNode, randomNode):     # connect only if the nodes are not connected either way
-            if not graph.has_edge(randomNode, fromNode):  # connect only if the nodes are not connected either way
+        if not graph.has_edge(randomNode, toNode):     # connect only if the nodes are not connected either way
+            if not graph.has_edge(toNode, randomNode):  # connect only if the nodes are not connected either way
                 if len(oxnet.getRandomVarParents(randomNode, graph))==0:  # allow one parent for now
                     tentativeGraph = bestMoveGaph.copy()
-                    tentativeGraph.add_edge(fromNode, randomNode)
+                    tentativeGraph.add_edge(randomNode, toNode)
                     tentativeScore = oscore.getScore(tentativeGraph, dataframe, attempt)
                     currentBestScore = oscore.getScore(bestMoveGaph, dataframe, attempt)
                     delta = int(tentativeScore) - int(currentBestScore)
                     print ">Evaluating new graph for better score: from=" + str(int(currentBestScore)) + " to=" + str(int(tentativeScore)) + \
-                          " GAIN=" + str(delta) + " adding EDGE=" + str(fromNode) +"-"+ str(randomNode) + \
+                          " GAIN=" + str(delta) + " adding EDGE=" + str(randomNode) +"-"+ str(toNode) + \
                           "TO =>NODES: " + str(tentativeGraph.nodes()) + ", =>EDGES: " + str(tentativeGraph.edges())
                     if int(delta) > int(minScoreGain):
                         cycles = list(nx.simple_cycles(tentativeGraph))
